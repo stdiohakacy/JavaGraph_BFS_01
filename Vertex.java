3d078fbc6adfddb225fbf4b8bbc46a8fc6d0b8dd
Duy <huynhduytam2210@gmail.com>
2020-01-09 16:03:24 +0700

    example

---
+++ Vertex.java
@@ -0,0 +1,50 @@
+package BFS._01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vertex {
+	private int data;
+	private boolean visited;
+	private List<Vertex> neighbourList;
+
+	public Vertex(int data) {
+		super();
+		this.data = data;
+		this.neighbourList = new ArrayList<>();
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public boolean isVisited() {
+		return visited;
+	}
+
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+
+	public List<Vertex> getNeighbourList() {
+		return neighbourList;
+	}
+
+	public void setNeighbourList(List<Vertex> neighbourList) {
+		this.neighbourList = neighbourList;
+	}
+
+	@Override
+	public String toString() {
+		return "Vertex [data=" + data + "]";
+	}
+	
+	public void addNeighbourVertex(Vertex vertex) {
+		this.neighbourList.add(vertex);
+	}
+
+}
